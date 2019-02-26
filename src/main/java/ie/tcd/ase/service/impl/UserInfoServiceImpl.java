35567bf48a5abcc7c69541feb243116e756f6199
flyingseal66 <2576209723>
2019-02-26 13:25:24 +0000

    add the implementation of UserInfoService

--- src/main/java/ie/tcd/ase/service/impl/UserInfoServiceImpl.java
+++ src/main/java/ie/tcd/ase/service/impl/UserInfoServiceImpl.java
@@ -1,16 +1,26 @@
 package ie.tcd.ase.service.impl;
 
+import ie.tcd.ase.dao.UserInfoMapper;
 import ie.tcd.ase.entity.UserInfo;
 import ie.tcd.ase.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserInfoServiceImpl implements UserInfoService {
+
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
     @Override
     public int insertUserInfo(UserInfo userInfo) {
+        userInfoMapper.insert(userInfo);
         return 0;
     }
 
     @Override
     public UserInfo selectByUserId(int id) {
-        return null;
+        UserInfo record = userInfoMapper.selectByPrimaryKey(id);
+        return record;
     }
 }
