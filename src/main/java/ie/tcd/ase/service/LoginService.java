6808802daae05d6c4c3846fb808a1326fd4f6aa3
JIAmengxuan <1092236342@qq.com>
2019-03-05 16:41:01 +0000

    The first version interfaces

--- src/main/java/ie/tcd/ase/service/LoginService.java
+++ src/main/java/ie/tcd/ase/service/LoginService.java
@@ -1,4 +1,13 @@
 package ie.tcd.ase.service;
 
 public interface LoginService {
+
+    /**
+     * For the login page. Check if the phone number and password matches.
+     * @param phoneNum The input phone number
+     * @param password The input password
+     * @return Return true if the @param phoneNum and @param password could match.
+     */
+    boolean checkUser(String phoneNum, String password);
+
 }
--- src/main/java/ie/tcd/ase/service/UserInfoService.java
+++ src/main/java/ie/tcd/ase/service/UserInfoService.java
@@ -4,8 +4,18 @@ import ie.tcd.ase.entity.UserInfo;
 
 public interface UserInfoService {
 
+    /**
+     * For sign in. Insert the user's information into the database.
+     * @param userInfo user's information.
+     * @return The number of records that are inserted into the database.
+     */
     int insertUserInfo(UserInfo userInfo);
 
+    /**
+     * For the profile page. Select the user's profile from the database.
+     * @param id The unique op the user.
+     * @return User's information
+     */
     UserInfo selectByUserId(int id);
 
 }
